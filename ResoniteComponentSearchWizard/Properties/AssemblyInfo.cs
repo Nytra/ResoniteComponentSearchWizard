58b3a12d697f0f14920d92146773b54af51ed677
Nytra <14206961+Nytra@users.noreply.github.com>
2024-03-12 17:49:16 +0000

    Version bump

--- ResoniteComponentSearchWizard/Properties/AssemblyInfo.cs
+++ ResoniteComponentSearchWizard/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nytra")]
 [assembly: AssemblyProduct("ComponentSearchWizard")]
-[assembly: AssemblyCopyright("Copyright © Nytra 2023")]
+[assembly: AssemblyCopyright("Copyright © Nytra 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
